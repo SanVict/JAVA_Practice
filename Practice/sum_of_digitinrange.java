49e5d2c05d22f941cd5109e56430f74f6a5f6065
Sanjay S <sanjaysureshk6@gmail.com>
2026-03-17 20:56:19 +0530

    8th program

--- Practice/sum_of_digitinrange.java
+++ Practice/sum_of_digitinrange.java
@@ -6,12 +6,12 @@ public class sum_of_digitinrange {
        Scanner sc=new Scanner(System.in);
 	     int a=sc.nextInt(); 
        int b=sc.nextInt();
-       if(b>a){int t=a;a=b;b=t; }
+       if(b>a){int t=a;a=b;b=t;}
        excludeboundary(a, b);
        includeboundary(a, b); 
  }
 static int excludeboundary(int a,int b) {
-      int out=(a*(a+1)/2) - (b*(b-1)/2);
+      int out=(a*(a+ 1)/2) - (b*(b-1)/2); 
       System.out.println("Boundary included sum: "+out);
       return 0;
      }
