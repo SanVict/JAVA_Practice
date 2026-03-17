7694814549cbc6b7c1073d16a28144188a3d9b2f
Sanjay S <sanjaysureshk6@gmail.com>
2026-03-17 19:21:14 +0530

    Adding Subscriptions

---
+++ Practice/Subscriptions.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Subscriptions
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{   
+	    Scanner sc=new Scanner(System.in);
+	    int t=sc.nextInt();
+	    while(t-->0){
+	        int N=sc.nextInt();
+	        int X=sc.nextInt();
+	    
+		System.out.println((N+5)/6*X);}
+
+	}
+}
