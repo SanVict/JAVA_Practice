9ee85edae9bbfe483de9893473ef56a87a295bfb
Sanjay S <sanjaysureshk6@gmail.com>
2026-03-17 20:50:49 +0530

    10th Program

--- Practice/reverse_the_number.java
+++ Practice/reverse_the_number.java
@@ -5,8 +5,8 @@ public class reverse_the_number {
        Scanner sc=new Scanner(System.in); 
        int n=sc.nextInt();
        int sum=0;
-       while(n!=0){
-        int d=n%10;
+       while(n!=0){        // use n !=0 for both -ve and +ve test case will satisfy
+        int d=n%10;        // Using string also we can done it 
         sum=(sum*10)+d;
         n/=10;
        }
