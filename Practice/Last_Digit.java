c6b2abc9982ecfb23674369b178354400f47015e
Sanjay S <sanjaysureshk6@gmail.com>
2026-03-17 19:43:27 +0530

    5th program

---
+++ Practice/Last_Digit.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Last_Digit {
+    public static void main(String[] Sanjay){
+     Scanner sc=new Scanner(System.in);
+	    int n=sc.nextInt();
+	    System.out.println(" The last digit is :" +(n%10));
+ }
+}
