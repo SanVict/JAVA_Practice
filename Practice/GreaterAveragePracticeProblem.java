77ae55a1196b8aabce18e543a70aa1efc144dd36
Sanjay S <sanjaysureshk6@gmail.com>
2026-03-17 19:11:17 +0530

    Initial commit - Balaji DSA

---
+++ Practice/GreaterAveragePracticeProblem.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GreaterAveragePracticeProblem
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+      Scanner sc=new Scanner(System.in);
+      int t=sc.nextInt();
+      while(t-->0){
+          double a= sc.nextDouble();
+          double b= sc.nextDouble();
+          double c= sc.nextDouble();
+          double avg=(a+b)/2;
+          if(avg>c) System.out.println("YES");
+          else 
+          System.out.println("NO");
+      }
+
+	}
+}
