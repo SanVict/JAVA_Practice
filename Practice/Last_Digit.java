544b03857fdeca95dbdbc7c064a0c72d040e8af6
Sanjay S <sanjaysureshk6@gmail.com>
2026-03-17 19:46:26 +0530

    5th program

--- Practice/Last_Digit.java
+++ Practice/Last_Digit.java
@@ -2,8 +2,9 @@ import java.util.*;
 
 public class Last_Digit {
     public static void main(String[] Sanjay){
-     Scanner sc=new Scanner(System.in);
+     Scanner sc=new Scanner(System.in); // Also use Logbase 10(n) to find the last digit 
+                                        //  The above opt will be in float use floor to round off
 	    int n=sc.nextInt();
 	    System.out.println(" The last digit is :" +(n%10));
- }
+}
 }
