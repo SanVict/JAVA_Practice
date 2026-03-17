24781c77a620d7fd72a54147972f234f8f9a26e6
Sanjay S <sanjaysureshk6@gmail.com>
2026-03-17 21:18:38 +0530

    12 th program

---
+++ Practice/GCD.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class GCD {
+    public static void main(String[] Sanjay){
+       Scanner sc=new Scanner(System.in); 
+       int a=sc.nextInt();
+       int b=sc.nextInt();
+      while(a>0 && b>0){
+        if(a>b) a=a%b;
+        else b=b%a;
+       }if(a==0)
+       System.out.println(b);
+       else System.out.println(a);
+    }
+}
