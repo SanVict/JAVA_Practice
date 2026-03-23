9eba04a9b413dcd3dac3bb62ad9905b2f8eb4427
Sanjay S <sanjaysureshk6@gmail.com>
2026-03-23 23:26:14 +0530

    Sum of array

---
+++ Array/sum_of_array.java
@@ -0,0 +1,11 @@
+import java.util.*;
+ class sum_of_array{
+    public static void main(String []sanjay){
+        int sum=0;
+        int []n={10,20,30,40,50,60};
+        for(int i=0;i<n.length;i++){
+            sum+=n[i];
+        }
+        System.out.println(sum);
+    }
+ }
