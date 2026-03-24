0aededae2c31d921a706a8df94e0b643850a11ec
Sanjay S <sanjaysureshk6@gmail.com>
2026-03-24 20:46:17 +0530

    left rotate array by one

---
+++ Array/left_rotate_arr_by_one.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class left_rotate_arr_by_one {
+     public static void main(String[] sanjay){
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0){
+        int n=sc.nextInt();
+        int []arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int temp=arr[0];
+        for(int i=1;i<n;i++){
+            arr[i-1]=arr[i];
+        }
+        arr[n-1]=temp;
+        System.out.println(Arrays.toString(arr));
+    }
+   }
+}
